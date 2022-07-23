64ce0e7ebae6fd111a20c9cc97373bd9fdf32231
Brian <brianteetersdesign@gmail.com>
2022-07-23 15:13:21 -0400

    Modified templates to display items from database.

--- app/views.py
+++ app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Item
 
 # Create your views here.
 
 
 def item_list(request):
-    return render(request, 'app/item_list.html', {})
+    items = Item.objects.filter(save_date__lte=timezone.now()).order_by('name')
+    return render(request, 'app/item_list.html', {'items': items})
