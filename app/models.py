b285b6e8b4e39e29a0cc22c472c316d8d0a48755
Brian <brianteetersdesign@gmail.com>
2022-07-23 14:28:00 -0400

    Rainbow Bakery App first commit

---
+++ app/models.py
@@ -0,0 +1,20 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    save_date = models.DateTimeField(blank=True, null=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+
+    def saveItem(self):
+        self.save_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.name
