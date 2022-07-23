0f22b2a72489c4cbec6bf1fdfdb8d5b8306e918c
Brian <brianteetersdesign@gmail.com>
2022-07-23 17:19:05 -0400

    Added ability to add buttons to main page via a new page.

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.item_list, name='item_list'),
+    path('item/new/', views.item_new, name='item_new'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Item
+from .forms import ItemForm
+from django.shortcuts import redirect
 
 # Create your views here.
 
@@ -9,3 +11,17 @@ def item_list(request):
     items = Item.objects.filter(
         save_date__lte=timezone.now()).order_by('name').order_by('category')
     return render(request, 'app/item_list.html', {'items': items})
+
+
+def item_new(request):
+    if request.method == "POST":
+        form = ItemForm(request.POST)
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.user = request.user
+            item.save_date = timezone.now()
+            item.save()
+            return redirect('/', pk=item.pk)
+    else:
+        form = ItemForm()
+    return render(request, 'app/item_edit.html', {'form': form})
