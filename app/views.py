6275e6bbef3ca0f254915d57dd4367b2dfa8d31f
Brian <brianteetersdesign@gmail.com>
2022-07-24 12:27:26 -0400

    Added edit page and detail page to make changes.

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Item
 from .forms import ItemForm
@@ -13,7 +13,14 @@ def item_list(request):
     return render(request, 'app/item_list.html', {'items': items})
 
 
+def item_detail(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    return render(request, 'app/item_detail.html', {'item': item})
+
+
 def item_new(request):
+    items = Item.objects.filter(
+        save_date__lte=timezone.now()).order_by('name').order_by('category')
     if request.method == "POST":
         form = ItemForm(request.POST)
         if form.is_valid():
@@ -21,7 +28,24 @@ def item_new(request):
             item.user = request.user
             item.save_date = timezone.now()
             item.save()
-            return redirect('/', pk=item.pk)
+            return redirect('/item/new/', pk=item.pk)
     else:
         form = ItemForm()
-    return render(request, 'app/item_edit.html', {'form': form})
+    return render(request, 'app/item_edit.html', {'form': form, 'items': items})
+
+
+def item_edit(request, pk):
+    items = Item.objects.filter(
+        save_date__lte=timezone.now()).order_by('name').order_by('category')
+    item = get_object_or_404(Item, pk=pk)
+    if request.method == "POST":
+        form = ItemForm(request.POST, instance=item)
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.user = request.user
+            item.save_date = timezone.now()
+            item.save()
+            return redirect('/item/new/', pk=item.pk)
+    else:
+        form = ItemForm(instance=item)
+    return render(request, 'app/item_edit.html', {'form': form, 'items': items})
