170b9ab1cb85463d8dae2df825666f56e037d9ca
Brian <brianteetersdesign@gmail.com>
2022-07-24 13:45:42 -0400

    Added login/logout buttons and authentication

--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from .models import Item
 from .forms import ItemForm
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -13,6 +14,7 @@ def item_list(request):
     return render(request, 'app/item_list.html', {'items': items})
 
 
+@login_required
 def item_detail(request, pk):
     items = Item.objects.filter(
         save_date__lte=timezone.now()).order_by('name').order_by('category')
@@ -30,6 +32,7 @@ def item_detail(request, pk):
     return render(request, 'app/item_edit.html', {'form': form, 'items': items})
 
 
+@login_required
 def item_new(request):
     items = Item.objects.filter(
         save_date__lte=timezone.now()).order_by('name').order_by('category')
@@ -46,6 +49,7 @@ def item_new(request):
     return render(request, 'app/item_edit.html', {'form': form, 'items': items})
 
 
+@login_required
 def item_edit(request, pk):
     items = Item.objects.filter(
         save_date__lte=timezone.now()).order_by('name').order_by('category')
